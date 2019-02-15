772210915c25875f6f71f0a323691f39c1dc518c
Patrick Olvera <patrickolvera@gmail.com>
2019-02-15 11:59:43 -0600

    test: ProductList.test Refactor

--- src/components/ProductList.test.js
+++ src/components/ProductList.test.js
@@ -2,35 +2,31 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import ProductList from './ProductList';
 
-it('should render a list of products as an unordered list', () => {
-  const mockProducts = [
+let mockProducts,
+    wrapper;
+
+beforeEach(() => {
+  // This is run before every test
+  mockProducts = [
     {id: 1, name: 'Mock Product 1', brand: 'MockBrandA'},
     {id: 2, name: 'Mock Product 2', brand: 'MockBrandB'},
     {id: 3, name: 'Mock Product 3', brand: 'MockBrandC'},
   ];
-  const wrapper = shallow(<ProductList products={mockProducts}/>);
+  wrapper = shallow(<ProductList products={mockProducts}/>);
+});
+
+it('should render a `<li>` for each product in props.products', () => {
+  
   expect(wrapper.find('li').length).toEqual(mockProducts.length); // 3
 });
 
 it('should display a product name in each `<li>` element', () => {
-  const mockProducts = [
-    {id: 1, name: 'Mock Product 1', brand: 'MockBrandA'},
-    {id: 2, name: 'Mock Product 2', brand: 'MockBrandB'},
-    {id: 3, name: 'Mock Product 3', brand: 'MockBrandC'},
-  ];
-  const wrapper = shallow(<ProductList products={mockProducts}/>);
   const firstElement = wrapper.find('li').first();
   // Check that the product name is contained within this element
   expect(firstElement.contains(mockProducts[0].name)).toEqual(true);
 });
 
 it('should display the brand name in each `<li>` element', () => {
-  const mockProducts = [
-    {id: 1, name: 'Mock Product 1', brand: 'MockBrandA'},
-    {id: 2, name: 'Mock Product 2', brand: 'MockBrandB'},
-    {id: 3, name: 'Mock Product 3', brand: 'MockBrandC'},
-  ];
-  const wrapper = shallow(<ProductList products={mockProducts}/>);
   const firstElement = wrapper.find('li').first();
   expect(firstElement.contains(mockProducts[0].brand)).toEqual(true);
 });
